bc770574bafbfb6e2b2296c8baf50982c7cc067a
adamyaN <adamsneupane@gmail.com>
2025-05-30 12:27:15 +0545

    update

--- Flutter/arithmetic_cubit/lib/view/arithmetic_home.dart
+++ Flutter/arithmetic_cubit/lib/view/arithmetic_home.dart
@@ -6,6 +6,8 @@ class ArithmeticHome extends StatelessWidget {
   final firstNumberController = TextEditingController();
   final secondNumberController = TextEditingController();
 
+  final Key myKey = GlobalKey();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -14,9 +16,52 @@ class ArithmeticHome extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            TextField(
-              inputFormatters: [],
-            )
+            Form(
+              key: myKey,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: TextFormField(
+                      controller: firstNumberController,
+                      validator: (value) {
+                        if (value!.isEmpty) return "Can't be empty";
+                        return null;
+                      },
+                      decoration: InputDecoration(label: Text("First Number")),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: TextFormField(
+                      controller: secondNumberController,
+                      validator: (value) {
+                        if (value!.isEmpty) return "Can't be empty";
+                        return null;
+                      },
+                      decoration: InputDecoration(label: Text("Second Number")),
+                    ),
+                  ),
+
+                  ElevatedButton(
+                    style: ButtonStyle(),
+                    onPressed: () {},
+                    child: Text("Add"),
+                  ),
+                  ElevatedButton(onPressed: () {}, child: Text("Subtract")),
+                  ElevatedButton(onPressed: () {}, child: Text("Multiply")),
+
+                  Text(
+                    "Output",
+                    style: TextStyle(fontSize: 48, fontWeight: FontWeight.bold),
+                  ),
+
+                  Text("state", style: TextStyle(fontSize: 20)),
+                ],
+              ),
+            ),
           ],
         ),
       ),
